bf148da51a3bac10a9c13c17f1b5ac7efd252581
Alexander Meinhardt <windyrain0212@outlook.com>
2018-06-11 06:50:44 +0200

    Removed JQuery dependency

--- jiss.js
+++ jiss.js
@@ -1,62 +1,88 @@
 /*
-    JISS - Javascript Image Slideshow Script | v1.0 | 18.10.2016
-    (https://github.com/windyrain0212/jiss)
-    (c) Alexander Meinhardt / @windyrain0212 / http://home.nssdev.net
+JISS - Javascript Image Slideshow Script | v1.0 | 18.10.2016
+(https://github.com/Avunia/JISS)
+(c) Alexander Meinhardt / @windyrain0212 / http://avunia.gitlab.io
 */
 
-var picturecount = 22;
+var picturecount = 8;
 var size = 3;
-var picturepath = './content/img/screenshots/';
+var picturepath = './assets/images/slideshow/';
 var extension = 'jpg';
 
-var nextpicture = null;    
+if (window.NodeList && !NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = function (callback, thisArg) {
+    thisArg = thisArg || window;
+    for (var i = 0; i < this.length; i++) {
+      callback.call(thisArg, this[i], i, this);
+    }
+  };
+}
+
+var nextpicture = null;
 var pictures = [];
 
-$(function() {
-    for (i=0;i<picturecount;i++) {
-        current = pad(i, size);
-        createBackgrounds(current);
-    }
-    
-    changeBackground();
-});
+document.addEventListener('DOMContentLoaded', function () {
+  for (i = 0; i < picturecount; i++) {
+    current = pad(i, size);
+    createBackgrounds(current);
+  }
+
+  changeBackground();
+}, false);
+
 function pad(num, size) {
-    var s = num+"";
-    while (s.length < size) s = "0" + s;
-    return s;
+  var s = num + "";
+  while (s.length < size) s = "0" + s;
+  return s;
 }
 
 function createBackgrounds(num) {
-    pictures[num] = new Image();
-    pictures[num] = picturepath + num + '.' + extension;
-    $('.background.container').append('<div class="background image'+num+'" style="background-image: url(\''+pictures[num]+'\'); opacity: 0;"></div>');
+  pictures[num] = new Image();
+  pictures[num] = picturepath + num + '.' + extension;
+  //$('.background.container').append('<div class="background image'+num+'" style="background-image: url(\''+pictures[num]+'\'); opacity: 0;"></div>');
+  Array.prototype.forEach.call(document.getElementsByClassName('jissBackground container'), function (entry) {
+    var ele = document.createElement('div');
+    ele.setAttribute('class', 'jissBackground image' + num);
+    ele.setAttribute('style', 'background-image: url(\'' + pictures[num] + '\');');
+    entry.appendChild(ele);
+  });
 }
 
 function changeBackground() {
-    // Just so it doesn't error on page load.
-    if(nextpicture !== null) {
-        // Hide current picture
-        $('.background.image'+pad(nextpicture, size)).animate({
-        opacity: 0.0
-    }, 750);
-    }
-    
-    prevpicture = nextpicture;
-    // Set new picture to load
-    nextpicture = Math.round(Math.random()*(picturecount-1));
-    // Check new value so it doesn't display the same image again
-    if(nextpicture == prevpicture) {
-        if(nextpicture < (picturecount-1)) {
-            nextpicture += 1;
-        } else if (nextpicture > 0) {
-            nextpicture -= 1;
-        }
-    }
-    // Show new picture
+  // Just so it doesn't error on page load.
+  if (nextpicture !== null) {
+    // Hide current picture
+    /*
     $('.background.image'+pad(nextpicture, size)).animate({
-        opacity: 1.0
+      opacity: 0.0
     }, 750);
+    */
+    Array.prototype.forEach.call(document.getElementsByClassName('jissBackground image' + pad(nextpicture, size)), function (entry) {
+      entry.className = entry.className.replace(/(?:^|\s)jissShown(?!\S)/g, '');
+    });
+  }
 
-    // Loop.
-    setTimeout("changeBackground()", 10000);
+  prevpicture = nextpicture;
+  // Set new picture to load
+  nextpicture = Math.round(Math.random() * (picturecount - 1));
+  // Check new value so it doesn't display the same image again
+  if (nextpicture == prevpicture) {
+    if (nextpicture < (picturecount - 1)) {
+      nextpicture += 1;
+    } else if (nextpicture > 0) {
+      nextpicture -= 1;
+    }
+  }
+  // Show new picture
+  /*
+  $('.background.image'+pad(nextpicture, size)).animate({
+    opacity: 1.0
+  }, 750);
+  */
+  Array.prototype.forEach.call(document.getElementsByClassName('jissBackground image' + pad(nextpicture, size)), function (entry) {
+    entry.className += ' jissShown';
+  });
+
+  // Loop.
+  setTimeout("changeBackground()", 10000);
 }
